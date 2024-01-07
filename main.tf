2677839beee341be947db593ec366f4090d42d02
Sweena <74145993+Sweena2002@users.noreply.github.com>
2024-01-07 20:52:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,19 @@ resource "aws_instance" "example" {
 }
 
 resource "aws_s3_bucket" "s3" {
-  bucket = "mytf_statebucket"                  
+  bucket = "my-tf-statebucket"                  
+}
 
+resource "aws_s3_bucket_ownership_controls" "s3" {
+  bucket = aws_s3_bucket.s3.id
+  rule {
+      object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+resource "aws_s3_bucket_acl" "s3" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3]
+
+  bucket = aws_s3_bucket.s3.id
+  acl    = "private"
 }
