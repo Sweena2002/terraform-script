8c23c70667ca0335b0e053f7a68bdf3a15d083ce
Sweena <74145993+Sweena2002@users.noreply.github.com>
2024-01-07 20:06:00 +0000

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,4 +1,9 @@
-resource "aws_s3_bucket" "mybucket_s3" {
-   bucket = "mybucket_s3"
-   acl = "private"
-  }
+provider "aws" {
+  region = "eu-west-2"  
+}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "aws_s3_mybucket_s3.id" 
+  acl    = "private"                  
+
+}
