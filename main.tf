2bbef0a19792cbc3a9b1be4e0f2443366914d9c4
Sweena <74145993+Sweena2002@users.noreply.github.com>
2024-01-07 22:41:36 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,20 +23,14 @@ resource "aws_instance" "example" {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "test-tftstate"
+    key    = "terraform.tfstate"
+  }
+}
 resource "aws_s3_bucket" "s3" {
-  bucket = "my-tf-statebucket"                  
+  bucket = "test-tftstate"                  
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3" {
-  bucket = aws_s3_bucket.s3.id
-  rule {
-      object_ownership = "BucketOwnerPreferred"
-    }
-}
 
-resource "aws_s3_bucket_acl" "s3" {
-  depends_on = [aws_s3_bucket_ownership_controls.s3]
-
-  bucket = aws_s3_bucket.s3.id
-  acl    = "private"
-}
