ee790a5e337bf19e4af65cd4e90c30543eb93fa7
Sweena <74145993+Sweena2002@users.noreply.github.com>
2024-01-18 14:45:59 +0000

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -9,20 +9,10 @@ module "eks" {
 
 module "eks_node_group" {
   source          = "terraform-aws-modules/eks/aws//modules/node_group"
-  cluster_name    = module.eks_cluster.my-first-eks-cluster
-  cluster_id      = module.eks_cluster.cluster_id
+  cluster_name    = module.eks.my-first-eks-cluster
   subnet_ids      = [aws_subnet.pvt_subnet.id]
   desired_capacity = 2
   max_capacity     = 3
   min_capacity     = 1
   instance_type    = "t2.micro"
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_type = "t2.micro"
-    }
-  }
 }
