0e14ecfeb30d9e3fc62d60dfb160fc41771435af
Sweena <74145993+Sweena2002@users.noreply.github.com>
2024-01-18 14:58:03 +0000

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-first-eks-cluster"
-  subnets_ids         = ["aws_subnet.pvt_subnet.id"]
+  subnet_ids         = ["aws_subnet.pvt_subnet.id"]
   vpc_id          = "aws_vpc.main_vpc.id"
   cluster_version = "1.21"
 }
