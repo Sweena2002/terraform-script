fa3cc001adf42d9d93f4cd41c98877410ac01a5d
Sweena <74145993+Sweena2002@users.noreply.github.com>
2024-01-30 06:42:39 +0000

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -38,7 +38,7 @@ resource "aws_vpc" "main_vpc" {
 # Create subnet within VPC
 resource "aws_subnet" "pvt_subnet1"{
     vpc_id = aws_vpc.main_vpc.id
-    cidr_block = "10.0.0.0/26"
+    cidr_block = "10.0.0.63/26"
     
 }
 
